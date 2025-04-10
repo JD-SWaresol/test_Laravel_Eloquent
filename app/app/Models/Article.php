211b7e2b151184c8d564f95365d6aed84e6cf6c7
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-04-10 12:42:30 -0600

    Claves UUID Parte 1

--- app/app/Models/Article.php
+++ app/app/Models/Article.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /**
+     * Las Claves UUID son Identificadores Alfanumericos y para poder hacer uso de ellos
+     * necesitamos el paquete 'HasUuids'.
+     * 
+     * Los UUID son mas eficientes para bases de datos indexadas, pues se pueden ordenar 
+     * de forma 'lexicograficamente'.
+     */
+
+
+    // Genera un UUID con marca de tiempo inicial que puede almacenarse eficientemente en una columna indexada de la base de datos. 
+    use HasUuids;
+
+    // ...
 }
+
+$article = Article::create(['title' => 'Traveling to Europe']);
+
+$article->id; // "8f8e8478-9035-4d23-b9a7-62f4d2612ce5"
