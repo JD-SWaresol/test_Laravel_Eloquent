3ebf30f96b7020f9a6b960c16e2e7e234456c726
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-04-10 12:05:03 -0600

    Especificacion de Nombre de Tabla Flight

--- app/app/Models/Flight.php
+++ app/app/Models/Flight.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+
+     //Especificamos el nombre de la tabla del modelo de forma manual usando $table
+     protected $table = 'my_flights';
 }
