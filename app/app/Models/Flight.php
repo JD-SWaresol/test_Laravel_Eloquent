9efffca87734e25c4e360bd7aefd52cfb58f3605
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-04-11 12:48:21 -0600

    Conexiones de Bases de Datos

--- app/app/Models/Flight.php
+++ app/app/Models/Flight.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    
+    /**
+     * Con Eloquent todos los modelos usan una conexión a base de datos configurada de forma predeterminada.
+     * 
+     * Si necesutamos especificar alguna conexión en especifico con algún modelo en particular, definimos la 
+     * propiedad '$connection'.
+     */
+
+               
+
+    /** The database connection that should be used by the model.
+     *
+     *@var string
+     */
+     protected $connection = 'mysql';
 }
