6394b1a5ae7a15cef05422b88fb774ed57937c14
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-04-11 12:35:33 -0600

    Marcas de tiempo -> Parte 2

--- app/app/Models/Flight.php
+++ app/app/Models/Flight.php
@@ -15,11 +15,21 @@ class Flight extends Model
      */
 
      /**
+      * Con la propiedad '$dateFormat' podemos personalizar el formato de las marcas de tiempo de un modelo. 
+      */
+
+     /**
       * Indicates if the model should be timestamped.
       *
       * @var bool
+      *
+      * The storage format of the model's date columns.
+      * @var string
       */
 
      // Gestion automatica de Eloquent inactiva
      public $timestamps = false;
+
+     //Esta propiedad determnina como almacenar los atributos de fecha en la base de datos, así como su tipo de formato cuando el modelo se serializa en una matriz o JSON.
+     protected $dateFormat = 'U';
 }
